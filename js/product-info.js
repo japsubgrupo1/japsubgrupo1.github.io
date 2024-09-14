a1f5174950392dd21b43dd5b95e4b2f79f74682a
Ernesto Argenzio <ernemogilevich@gmail.com>
2024-09-14 20:13:24 -0300

    Comments + fix when image is clicked when width is less than 768px

--- js/product-info.js
+++ js/product-info.js
@@ -39,19 +39,26 @@ function setProdID(id) {
     window.location.href = "product-info.html"; //Redirigimos a product-info.html
 }
 
+//Función para cambiar la imagen una vez clickeada, recibe por parámetro el URL de la imagen clickeada
 function imgSwap(clickedImg) {
-    prodImageMain.src = clickedImg.src;
+    prodImageMain.src = clickedImg.src; //Cambia la ruta de la imagen principal por la de la imagen clickeada
 }
 
+//Función para hacer zoom
 function zoom() {
-    prodImageMain.style.transform = "scale(1.5)";
-    prodImageMain.style.objectFit = "contain";
-    prodImageMain.style.border = "0";
-    prodImageMain.style.height = "auto";
-    prodImageMain.style.margin = "16% 0 0 25%";
-    imgBtn.style.display = "block";
+    if(window.innerWidth <= 768) { //Caso en el que el ancho de la pantalla es igual o menor a 768px (tablet / celular)
+        null; //Aquí se hará una función para agrandar la imagen de alguna forma aún no decidida, ya que de momento se ve bien en el dispositivo
+    } else { //Caso contrario (escritorio)
+        prodImageMain.style.transform = "scale(1.5)"; //Agrandar imagen
+        prodImageMain.style.objectFit = "contain"; //Mostrar imágen en relación de aspecto original (cover / panorámica)
+        prodImageMain.style.border = "0"; //Eliminamos el borde
+        prodImageMain.style.height = "auto"; //Mostramos el alto total de la imágen
+        prodImageMain.style.margin = "16% 0 0 25%"; //Movemos la imagen
+        imgBtn.style.display = "block"; //Mostramos el botón de cerrado
+    }
 }
 
+//Función para quitar el zoom cuando se aprieta el botón, revirtiendo los cambios de zoom() (no encontramos mejor forma de hacer esto aún)
 function unzoom() {
     prodImageMain.style.transform = "none";
     prodImageMain.style.objectFit = "cover";
@@ -61,15 +68,21 @@ function unzoom() {
     imgBtn.style.display = "none";
 }
 
+//Acciones a realizar apenas la página se carga
 document.addEventListener("DOMContentLoaded", function (e) {
+    //Definimos productUrl, el cual va generar una URL comenzando con PRODUCT_INFO_URL (https://japceibal.github.io/emercado-api/products/, definida en init.js)
+    //A esta variable se le agrega el valor de la key prodID almacenada en localStorage, la cual define la función setProdID, en products.js (línea 40)
+    //A esto finalmente le añadimos el .json, tomándolo de EXT_TYPE para hacerlo más fancy (definido en init.js)
+    //Un ejemplo de URL generado puede ser el del Chevrolet Onix Joy, dando como resultado https://japceibal.github.io/emercado-api/products/50921.json
     const productUrl = `${PRODUCT_INFO_URL}${localStorage.getItem("prodID")}${EXT_TYPE}`;
 
+    //Traemos los datos
     getJSONData(productUrl).then(function (resultObj) {
-        if (resultObj.status === "ok") {
-            product = resultObj.data;
-            showProduct();
-        } else {
-            console.error(`Error durante el fetch a ${productUrl}, puede que el recurso no esté disponible.`);
+        if (resultObj.status === "ok") { //Acción cuando la promesa da "ok"
+            product = resultObj.data; //product toma todos los elementos del array del producto al que se ingresa, encontrado en la data de la respuesta de la promesa
+            showProduct(); //Mostrar elementos en pantalla
+        } else { //Acción cuando la promesa no da "ok"
+            console.error(`Error durante el fetch a ${productUrl}, puede que el recurso no esté disponible.`); //Loggear error en consola
         }
     });
 
